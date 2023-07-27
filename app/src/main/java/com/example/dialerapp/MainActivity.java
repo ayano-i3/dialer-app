ba90d59d31f03b89546ead2f220abb8bfb9293a6
Okuda Ayano <ayano.o@i3-systems.com>
2023-07-27 13:30:06 +0900

    viewBindingを追加

--- app/src/main/java/com/example/dialerapp/MainActivity.java
+++ app/src/main/java/com/example/dialerapp/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.dialerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.example.dialerapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
     }
 }
